6575f084dc72c12b6b0ab9ab8b2972064e4a5a86
手川武海 <tegawatakemi@tegawatakeshikainoMacBook-Air.local>
2026-07-06 23:49:34 +0900

    firebaseの設定を追加

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -7,12 +7,12 @@
 // ------------------------------------------------------------
 
 window.FIREBASE_CONFIG = {
-  apiKey:            "ここにapiKeyを貼る",
-  authDomain:        "ここにauthDomainを貼る",
-  projectId:         "ここにprojectIdを貼る",
-  storageBucket:     "ここにstorageBucketを貼る",
-  messagingSenderId: "ここにmessagingSenderIdを貼る",
-  appId:             "ここにappIdを貼る"
+  apiKey:            "AIzaSyCH79YY8SuTgwny4A2BcYecA_zVlSaLNwI",
+  authDomain:        "openingproject-d5390.firebaseapp.com",
+  projectId:         "openingproject-d5390",
+  storageBucket:     "openingproject-d5390.firebasestorage.app",
+  messagingSenderId: "644252594737",
+  appId:             "1:644252594737:web:326a7277c96dfca5cf5b10"
 };
 
 // 実務ビューで保存するときの簡易合言葉（任意）。
